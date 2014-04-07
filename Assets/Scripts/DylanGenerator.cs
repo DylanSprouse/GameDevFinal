b7f9325e3c6ba0ae7d0e34672860eaaac62b855e
Dylan Sprouse <dts264@nyu.edu>
2014-04-07 14:55:00 -0400

    Added a "Forest Pulse" script as well as a couple new asset folders

--- Assets/Scripts/DylanGenerator.cs
+++ Assets/Scripts/DylanGenerator.cs
@@ -8,19 +8,59 @@ public class DylanGenerator : MonoBehaviour {
 
 	int count = 0;
 
+	private bool PulseDelay = false;
+
 	// Use this for initialization
 	void Start () {
+
+		StartCoroutine (ForestPulse() );
 	
 	}
-	
+
+	IEnumerator ForestPulse() {
+
+		yield return new WaitForSeconds ( 5f) ;
+		PulseDelay = false;
+
+		if (count < 500){
+					
+
+			
+						//spawn a skyscraper
+						Instantiate ( pinetreeprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f,100f ) ), Quaternion.identity );
+					
+						//spawn a shopping mall
+						Instantiate ( hollyprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f, 100f ) ), Quaternion.identity );
+							
+			
+						Instantiate ( pinesproutprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f, 100f ) ), Quaternion.identity );
+			
+						Instantiate ( pinesproutprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f, 100f ) ), Quaternion.identity );
+			
+						Instantiate ( snowmanprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f, 100f ) ), Quaternion.identity );
+							
+						Instantiate ( logcabinprefab, new Vector3( Random.Range ( -100f, 100f ), 0f, Random.Range ( -100f, 100f ) ), Quaternion.identity );
+							
+						
+		}
+		}
+
+
+
 	// Update is called once per frame
 	void Update () {
+
+		if (PulseDelay == false){
+
+			StartCoroutine (ForestPulse() );
+			PulseDelay = true ;
+		}
 
 //		if (count = 0){
 //
 //		}
 
-//		if (count < 500){
+		if (count < 500){
 //		int randomnumber = Random.Range (0, 101);
 //
 //		if (randomnumber < 35) {
@@ -54,5 +94,6 @@ public class DylanGenerator : MonoBehaviour {
 //				
 //		}
 //	}
-	}
+}
+}
 }
