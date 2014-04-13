e98cc5f7d8cbe2ddb2acca9492774838251c499d
Lauren Gutlohn <lgutlohn@gmail.com>
2014-04-13 14:26:01 -0400

    cameraShake for earthquake

---
+++ Assets/Scripts/ScreenShaker.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScreenShaker : MonoBehaviour {
+	
+	public float shakeStrength = 2f;
+	public float shake = 0.5f;
+
+	Vector3 originalPosition;
+	
+	void Start()
+	{
+		originalPosition = transform.localPosition;
+	}
+	
+	void Update()
+	{
+		if(Input.GetMouseButton (0) || (Input.GetMouseButton (1)))
+		{
+			shake = shakeStrength;
+		}
+		
+		Camera.main.transform.localPosition = originalPosition + (Random.insideUnitSphere * shake);
+		
+		shake = Mathf.MoveTowards(shake, 0, Time.deltaTime * shakeStrength);
+		
+		if(shake == 0)
+		{
+			Camera.main.transform.localPosition = originalPosition;
+		}
+	}
+	
+}
