28e24401a41d206b4680924cbaa4d99cb70cf5a8
Dylan Sprouse <dts264@nyu.edu>
2014-04-25 12:58:17 -0400

    General update commit

--- Assets/Scripts/FloodGenerator.cs
+++ Assets/Scripts/FloodGenerator.cs
@@ -9,12 +9,11 @@ public class FloodGenerator : MonoBehaviour {
 	public GameObject rain;
 	GameObject rainPrefabClone;
 	public float time = 5f;
-<<<<<<< HEAD
-=======
+
 	public int waterHeight = 30;
 
 	public AudioSource rainSound;
->>>>>>> GitHub/dillon_branch
+
 	
 	// Use this for initialization
 	void Start () {
@@ -31,25 +30,24 @@ public class FloodGenerator : MonoBehaviour {
 				int randomHex = Random.Range (0, GetComponent<MouseController>().greenTileList.Count);
 				GameObject current = GetComponent<MouseController>().greenTileList[randomHex];
 				Vector3 hexPos = new Vector3(current.transform.position.x, current.transform.position.y, current.transform.position.z);
-<<<<<<< HEAD
-=======
+
 				Vector3 hexPos1 = new Vector3(current.transform.position.x, (current.transform.position.y)+waterHeight, current.transform.position.z);
->>>>>>> GitHub/dillon_branch
+
 				GetComponent<MouseController>().greenTileList.Remove (current);
 				GetComponent<MouseController>().builtTileList.Remove (current);
 				GameObject.Destroy(current);
 				//Instantiate(blueTile, hexPos, Quaternion.identity);
-<<<<<<< HEAD
+
 				rainPrefabClone = Instantiate(rain, hexPos, Quaternion.identity) as GameObject;
 				Instantiate(blueTile, hexPos, Quaternion.identity);
 				Destroy(rainPrefabClone, time);
 				//rain.Play();
-=======
+
 				rainPrefabClone = Instantiate(Resources.Load ("RainPrefab", typeof (GameObject)), hexPos1, Quaternion.identity) as GameObject;
 				Instantiate(blueTile, hexPos, Quaternion.identity);
 				Destroy(rainPrefabClone, time);
 				rainSound.Play();
->>>>>>> GitHub/dillon_branch
+
 			}
 		}
 	}
