4dd5957496f31d415514744201b1c927297f6978
Dylan Sprouse <dts264@nyu.edu>
2014-05-20 13:42:41 -0400

    Committing merges

---
+++ Assets/Scripts/EndScore.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScore : MonoBehaviour {
+
+	//private MouseController mouse;
+	public GUIText endScore;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		endScore.text = "Score: " + (MouseController._day * (MouseController.hubrisAmount)/10);
+		//endScore.text = "Score: " + (mouse.dayTimer.text + mouse.hubrisCounter.text);
+
+	}
+}
